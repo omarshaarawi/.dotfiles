150e85f587297b9156d2a012b4a4491442cf2388
Omar <omar25shaarawi@gmail.com>
2024-08-03 18:20:39 -0500

    feat: update plugins to their latest commits

--- .config/nvim/lua/shaarawi/lazy/copilot.lua
+++ .config/nvim/lua/shaarawi/lazy/copilot.lua
@@ -2,14 +2,22 @@ return {
     {
         "github/copilot.vim",
         config = function()
+            -- Disable Copilot by default
+            vim.g.copilot_enabled = 0
+
             vim.g.copilot_filetypes = {
                 ["TelescopePrompt"] = false,
             }
-
             local opts = { silent = true }
             vim.keymap.set("i", "<C-j>", "<Plug>(copilot-next)", opts)
             vim.keymap.set("i", "<C-k>", "<Plug>(copilot-previous)", opts)
             vim.keymap.set("i", "<C-l>", "<Plug>(copilot-suggest)", opts)
+
+            -- Add a command to toggle Copilot
+            vim.api.nvim_create_user_command("CopilotToggle", function()
+                vim.g.copilot_enabled = not vim.g.copilot_enabled
+                print("Copilot " .. (vim.g.copilot_enabled and "enabled" or "disabled"))
+            end, {})
         end,
     },
 }
