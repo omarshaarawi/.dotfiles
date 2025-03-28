d922daa31054432538fdfb0528336c410d6ff963
Omar <omar25shaarawi@gmail.com>
2025-03-27 21:59:24 -0500

    fix lsp signs deprecation notice

--- .config/nvim/lua/shaarawi/lazy/lsp.lua
+++ .config/nvim/lua/shaarawi/lazy/lsp.lua
@@ -81,18 +81,30 @@ return {
                 border = "rounded",
                 source = "always",
             },
-            signs = true,
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = " ",
+                    [vim.diagnostic.severity.HINT] = " ",
+                    [vim.diagnostic.severity.INFO] = " ",
+                },
+                numhl = {
+                    [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+                    [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+                    [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+                    [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+                },
+                texthl = {
+                    [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+                    [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+                    [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+                    [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+                },
+            },
             underline = true,
             update_in_insert = false,
             severity_sort = true,
         })
-        
-        -- Change diagnostic symbols in the sign column
-        local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-        end
         
         -- Add rounded borders to hover windows
         vim.o.winborder = "rounded"
